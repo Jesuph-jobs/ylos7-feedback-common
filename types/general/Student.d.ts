d541e5e47a8bec2ae990790db9d556abe3fb3124
Youcef Madadi <tahher@live.fr>
2024-06-09 03:36:27 +0100

    refactor: Update StudentI interface to include 'selected' property

--- types/general/Student.d.ts
+++ types/general/Student.d.ts
@@ -4,10 +4,14 @@ declare interface PublicStudentI {
 	lastName: string;
 	profilePicture?: string;
 }
+declare interface SelectedStudentI extends PublicStudentI {
+	selected: boolean;
+}
 declare interface BasicStudentI extends Omit<PublicStudentI, 'id'> {
 	email: string;
 	phone?: string;
 }
 declare interface StudentI<ID = string, TimeT extends string | Date = string> extends TimeStampI<TimeT>, BasicStudentI {
 	id: ID;
+	selected?: boolean;
 }
