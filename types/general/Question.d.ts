86bfe104523268e425acf09c62ae076abe8d7bbc
Youcef Madadi <tahher@live.fr>
2024-06-19 01:32:01 +0100

    refactor: Add color evaluations to QuestionInfoI interface

--- types/general/Question.d.ts
+++ types/general/Question.d.ts
@@ -3,11 +3,23 @@ declare interface QuestionsValuesI<V = number> {
 	quality: V;
 	frequency: V;
 }
+declare type ColorValueI =
+	| 'V' // Very bad
+	| 'B' // Bad
+	| 'A' // Average
+	| 'G' // Good
+	| 'E'; // Excellent
+declare interface QuestionEvaluationI {
+	color: ColorValueI;
+	text: string;
+}
+declare type QuestionEvaluationsArrayI<T = QuestionEvaluationI> = [T, T, T, T, T];
 declare type QuestionsValuesKeysI = keyof QuestionsValuesI;
 declare interface QuestionInfoI {
 	question: string;
 	title: string;
 	type: QuestionsValuesKeysI;
+	evaluations?: QuestionEvaluationsArrayI;
 }
 declare interface BasicQuestionI<QuestionID = string> extends QuestionInfoI {
 	sessionId: QuestionID;
