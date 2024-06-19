6ca61196c5d4a544bed13bb71a1a2aa01f572288
Youcef Madadi <tahher@live.fr>
2024-06-19 12:16:27 +0100

    refactor: Add color evaluations to QuestionInfoI schema

--- validators/models/question.ts
+++ validators/models/question.ts
@@ -1,17 +1,32 @@
+import { ZodType } from 'zod';
+
 import { MyZodType, z } from '^common/defaultZod';
-import { mongoIDSchema } from '^common/elements';
+import { arraySchema, mongoIDSchema } from '^common/elements';
 export const questionsTypeMap: Record<QuestionsValuesKeysI, string> = {
 	importance: 'importance',
 	quality: 'qualité',
 	frequency: 'fréquence',
 };
 export const questionsTypeArray = Object.keys(questionsTypeMap) as unknown as MyEnum<QuestionsValuesKeysI>;
+export const questionEvaluationSchema = ({
+	color,
+	text,
+}: Partial<Record<keyof QuestionEvaluationI, ErrorsSchemaMsgI>> = {}) =>
+	z
+		.object<MyZodType<QuestionEvaluationI>>({
+			color: z.enum<ColorValueI, MyEnum<ColorValueI>>(['V', 'B', 'A', 'G', 'E'], color),
+			text: z.string(text),
+		})
+		.openapi('Évaluation de la question', { description: 'Évaluation de la question' });
 export const questionInfoSchema = ({ question, title }: Partial<Record<keyof QuestionInfoI, ErrorsSchemaMsgI>> = {}) =>
 	z
 		.object<MyZodType<QuestionInfoI>>({
 			question: z.string(question),
 			title: z.string(title),
 			type: z.enum<QuestionsValuesKeysI, MyEnum<QuestionsValuesKeysI>>(questionsTypeArray),
+			evaluations: arraySchema(questionEvaluationSchema()).length(5).optional() as ZodType<
+				QuestionEvaluationsArrayI<QuestionEvaluationI> | undefined
+			>,
 		})
 		.openapi('Question de base', { description: 'Question de base' });
 
